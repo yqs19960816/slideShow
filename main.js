3cc904e7bc25cc007743d6e4986e2def1706e4e0
杨青松 <1260013987@qq.com>
2018-10-07 17:38:41 +0800

    状态切换无缝轮播

---
+++ main.js
@@ -0,0 +1,43 @@
+let n 
+// 初始化
+init()
+function x(n) {
+    if (n > 5) {
+        n = n % 5
+        if (n === 0) {
+            n = 5
+        }
+        //n = 1 2 3
+    }
+    return n
+}
+setInterval(() => {
+    makeLeave($('.images > img:nth-child('+x(n)+')'))
+        .one('transitionend', (e) => {
+            makeEnter($(e.currentTarget))
+        })
+        makeCurrent($(`.images > img:nth-child(${x(n+1)})`))
+    n += 1
+}, 2500)
+
+
+
+
+
+//封装函数
+function init(){
+    n = 1
+    makeCurrent(makeCurrent($(`.images > img:nth-child(${n})`)).siblings())
+}
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+    
+}
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+    
+}
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+
+}
